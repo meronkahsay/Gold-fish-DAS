9c5c44932402faba4216976667c8141c458eac1b
meronkahsay <kahsaymeront@gmail.com>
2025-05-07 08:26:23 +0300

    Created support detail table

---
+++ support_details.sql
@@ -0,0 +1,17 @@
+INSERT INTO malnutrition.support_detail (support_id,type_of_support,notes) VALUES
+	 ('s01','supplement','vitamin'),
+	 ('s02','feeding','3meal/day'),
+	 ('s03','medical','checkup costs'),
+	 ('s04','supplement','4meals/day'),
+	 ('s05','feeding','3meal/day'),
+	 ('s06','supplement','checkup costs'),
+	 ('s07','feeding','4meals/day'),
+	 ('s08','supplement','3meal/day'),
+	 ('s09','medical','checkup costs'),
+	 ('s10','supplement','4meals/day');
+INSERT INTO malnutrition.support_detail (support_id,type_of_support,notes) VALUES
+	 ('s11','feeding','4meals/day'),
+	 ('s12','feeding','3meal/day'),
+	 ('s13','medical','checkup costs'),
+	 ('s14','feeding','4meals/day'),
+	 ('s15','medical','3 meals/day');
