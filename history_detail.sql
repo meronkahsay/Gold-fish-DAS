71b0fd688d83cc583cb1535fbf69580e2e7c74b9
Linn Amondi <linnamondi@gmail.com>
2025-05-13 08:02:24 +0300

    Created History detail table

---
+++ history_detail.sql
@@ -0,0 +1,17 @@
+INSERT INTO malnutrition.history_detail (history_id,child_id,disease,date_recorded) VALUES
+	 ('h01','C01','Marasmus','2023-04-16'),
+	 ('h02','C02','Ricket','2020-01-23'),
+	 ('h03','C03','Beriberi','2021-10-18'),
+	 ('h04','C04','Marasmus','2021-01-15'),
+	 ('h05','C05','Ricket','2020-04-19'),
+	 ('h06','C06','Beriberi','2021-06-23'),
+	 ('h07','C07','Scurvy','2020-01-19'),
+	 ('h08','C08','Pellegra','2021-07-28'),
+	 ('h09','C09','Xerophthalia','2020-01-01'),
+	 ('h10','C10','Anemia','2020-03-29');
+INSERT INTO malnutrition.history_detail (history_id,child_id,disease,date_recorded) VALUES
+	 ('h11','C11','Marasmus','2020-12-27'),
+	 ('h12','C12','Kwashiokor','2021-10-29'),
+	 ('h13','C13','Marasmus','2020-05-26'),
+	 ('h14','C14','Anemia','2020-08-30'),
+	 ('h15','C15','Pallegra','2021-04-30');
